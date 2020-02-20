04e720c9290ff606b3cbf05758bfca740391d2c3
RudolfKlikh <rudolfklikh@gmail.com>
2020-02-20 11:55:41 +0100

    Layouts plus Animations

---
+++ messenger/src/app/shared/animations/components-animation.ts
@@ -0,0 +1,33 @@
+import { trigger, transition, useAnimation } from '@angular/animations';
+import { rotateInDownLeft, rotateInDownRight, zoomIn } from 'ng-animate';
+
+export const routerAnimations = trigger('routerAnimations', [
+  transition('login => registration', [
+    useAnimation(rotateInDownLeft, {
+      params: {
+        timing: 0.5
+      }
+    })
+  ]),
+  transition('registration => login', [
+    useAnimation(rotateInDownRight, {
+      params: {
+        timing: 0.5
+      }
+    })
+  ]),
+  transition('registration <=> reset', [
+    useAnimation(zoomIn, {
+      params: {
+        timing: 0.5
+      }
+    })
+  ]),
+  transition('login <=> reset', [
+    useAnimation(zoomIn, {
+      params: {
+        timing: 0.5
+      }
+    })
+  ]),
+]);
