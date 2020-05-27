39aea1920737cbcac12ec5d852f4b2bc2f33bd5f
RudolfKlikh <rudolfklikh@gmail.com>
2020-05-27 14:48:55 +0200

    Work-43, WORK-44

---
+++ messenger/src/app/shared/module/angular-material/angular-material.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+@NgModule({
+  declarations: [],
+  imports: [
+    MatSliderModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatSlideToggleModule
+  ],
+  exports: [
+    MatSliderModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatInputModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatDialogModule,
+    MatSlideToggleModule
+  ]
+})
+export class AngularMaterialModule { }
